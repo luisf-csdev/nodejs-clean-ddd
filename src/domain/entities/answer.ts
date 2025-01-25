6dc972427853fa54b249a9cad27b1e66be1739b9
Luís Felipe <luisf.csdev@gmail.com>
2025-01-25 18:02:11 -0300

    feat: ddd fundamentals

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,13 +1,61 @@
 import { Entity } from '@/core/entities/entity'
+import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import type { Optional } from '@/types/optional'
 
 interface AnswerProps {
+  authorId: UniqueEntityID
   content: string
-  authorId: string
-  questionId: string
+  questionId: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
   get content() {
     return this.props.content
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trim().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  static create(
+    props: Optional<AnswerProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const answer = new Answer(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id,
+    )
+
+    return answer
   }
 }
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Answer } from '../entities/answer'
 import type { AnswersRepository } from '../repositories/answers-repository'
 
@@ -11,11 +12,15 @@ export class AnswerQuestionUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
   async execute({
+    content,
     instructorId,
     questionId,
-    content,
   }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({ content, authorId: instructorId, questionId })
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    })
 
     await this.answersRepository.create(answer)
 
